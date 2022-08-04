267bf2f6ee50754abfad6cfe8e43cac3e9e50c6a
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-04 18:52:40 +0200

    Comando Select

--- src/main/java/com/udemy/db/DB.java
+++ src/main/java/com/udemy/db/DB.java
@@ -3,9 +3,7 @@ package com.udemy.db;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -47,5 +45,27 @@ public class DB {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public static void closeStatement(Statement st) {
+
+        if(st != null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
     }
 }
