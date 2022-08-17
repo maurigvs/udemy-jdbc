63a918d29c9626b4f14adc3b513f275558db55e7
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-17 00:08:26 -0300

    Packaging change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import org.example.db.DB;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        Connection conn = DB.getConnection();
+        DB.closeConnection();
     }
 }
